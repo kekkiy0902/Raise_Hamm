774e85f24e43ee12500be6004dbe3234c2925f4a
github kekkiy0902 <kekkiy9296@yahoo.co.jp>
2020-08-28 08:49:32 +0900

    レスポンシブほぼ完成

---
+++ js/jquery.js
@@ -0,0 +1,21 @@
+/*スマホ画面では、ヘッダーメニューをハンバーガーにする*/
+$(function(){
+        $("#open").show();
+        $("#close").hide();
+    // アイコンをクリック
+	$("#open").click(function(){
+		// ulメニューを開閉する
+		$("#navi").slideToggle();
+        $("#open").hide();
+        $("#close").show();
+	});
+        
+    // アイコンをクリック
+	$("#close").click(function(){
+		// ulメニューを開閉する
+		$("#navi").slideToggle();
+        $("#open").show();
+        $("#close").hide();
+	});
+	
+});
